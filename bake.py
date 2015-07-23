404bc2ae528113e3070c34d61f6cebc988a2e348
a.reshetnikov <a.reshetnikov@gaijin.ru>
2015-07-23 23:32:38 +0300

    Added bake.py build script

---
+++ bake.py
@@ -0,0 +1,10 @@
+import os
+import baker
+
+baker.compilerOutputDir = os.path.join(os.environ['HATCHERY_BUILDS'], 'lodepng')
+
+baker.compilerSources = [
+os.path.join(os.environ['HATCHERY_SOURCES'], 'lodepng', 'lodepng.cpp'),
+]
+
+baker.compile()
