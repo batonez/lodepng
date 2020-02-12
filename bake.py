b4eb4671e3358e4b6d0b71ce21fcbd6647b6cede
jebrax <batonez@gmail.com>
2020-02-12 21:15:09 +0300

    Updated bakefile, added .gitignore

--- bake.py
+++ bake.py
@@ -1,10 +1,13 @@
 import os
-import baker
+import importlib.util
+bakerModuleSpec = importlib.util.spec_from_file_location("baker", os.path.join(os.environ['HATCHERY_SOURCES'], 'baker', 'baker.py'))
+baker = importlib.util.module_from_spec(bakerModuleSpec)
+bakerModuleSpec.loader.exec_module(baker)
 
 baker.compilerOutputDir = os.path.join(os.environ['HATCHERY_BUILDS'], 'lodepng')
 
 baker.compilerSources = [
-os.path.join(os.environ['HATCHERY_SOURCES'], 'lodepng', 'lodepng.cpp'),
+os.path.join(os.environ['HATCHERY_VENDOR'], 'lodepng', 'lodepng.cpp'),
 ]
 
 baker.sanitizeBakeInput()
